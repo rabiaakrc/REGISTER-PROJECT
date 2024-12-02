4c51ad58d0c648715f091600a97414a89929476c
rabiaakrc <rabiakaraca33@gmail.com>
2024-12-03 00:29:29 +0300

    save

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
